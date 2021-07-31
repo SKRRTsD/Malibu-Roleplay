f4ca2233068ade0cc48e81cb89a48b4972c70c26
Evan-1889 <78846695+Evan-1889@users.noreply.github.com>
2021-07-31 23:27:36 +0100

    noice

---
+++ server-data/resources/[casino]/prp_slots/server.lua
@@ -0,0 +1,54 @@
+
+
+RegisterServerEvent("prp_slots:BetsAndMoney")
+AddEventHandler("prp_slots:BetsAndMoney", function(bets)
+	local src = source
+	local user = exports["prp-core"]:getModule("Player"):GetUser(src)
+        if bets % 50 == 0 and bets >= 50 then
+            if user:getCasino() >= bets then
+                user:removeCasino(bets)
+                TriggerClientEvent("DoLongHudText", src, bets)
+                TriggerClientEvent("prp_slots:UpdateSlots", src, bets)
+            else
+                TriggerClientEvent('DoLongHudText', src, "Not enought money")
+            end
+        else
+            TriggerClientEvent('DoLongHudText', src, "You have to insert a multiple of 50. ex: 100, 350, 2500")
+        end
+end)
+
+RegisterServerEvent("prp_slots:PayOutRewards")
+AddEventHandler("prp_slots:PayOutRewards", function(amount)
+	local src = source
+	local user = exports["prp-core"]:getModule("Player"):GetUser(src)
+        amount = tonumber(amount)
+        if amount > 0 then
+            user.addCasino(amount)
+            TriggerClientEvent('DoLongHudText', src, "Slots: You won $"..amount.." not bad at all!")
+        else
+            TriggerClientEvent('DoLongHudText',src, "Slots: Unfortunately you've lost all the money, maybe next time.")
+        end
+end)
+
+CasinoSlots = true
+RegisterServerEvent("slot:toggle")
+AddEventHandler('slot:toggle', function(pAuto)
+    if not pAuto then
+        local pSrc = source
+        TriggerClientEvent("DoLongHudText", pSrc, ('Spin To Win has been %s!'):format(CasinoSlots and 'Disabled' or 'Enabled')) 
+        CasinoSlots = not CasinoSlots
+    else
+        CasinoSlots = not CasinoSlots
+    end
+end)
+
+
+RegisterServerEvent("attempt:slot", function()
+    local pSrc = source
+    if CasinoSlots then
+        TriggerClientEvent("prp_slots:UpdateSlots", pSrc, true)
+    else
+        TriggerClientEvent("DoLongHudText", pSrc, "Spin To Win is disabled, please contact a Casino Dealer!")
+    end
+end)
+
