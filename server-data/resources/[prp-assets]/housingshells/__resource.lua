0b87f7f84856ead6647deb3c54f3ff603318bca5
Evan-1889 <78846695+Evan-1889@users.noreply.github.com>
2021-07-31 23:53:34 +0100

    damn breh!

---
+++ server-data/resources/[prp-assets]/housingshells/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
+files {
+    "interiorproxies.meta",
+    'playerhouse_hotel.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'playerhouse_hotel.ytyp'
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+data_file 'DLC_ITYP_REQUEST' 'shellprops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv5.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv9.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv10.ytyp'
+
