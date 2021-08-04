4579aa4bcddd193bc9511f5b0b02ae65f9ef5637
Evan-1889 <78846695+Evan-1889@users.noreply.github.com>
2021-08-04 13:07:02 +0100

    renames

--- server-data/resources/[PRP]/prp-drugs/server/sv_electronicrun.lua
+++
@@ -1,6 +0,0 @@
-RegisterServerEvent('electronicrunstart')
-AddEventHandler('electronicrunstart', function()
-local src = source
-local user = exports["mrp-core"]:getModule("Player"):GetUser(src)
-    user:removeMoney(550)
-end)
