5691fb2b16351d82d20427e011a49bf68152c3de
Evan-1889 <78846695+Evan-1889@users.noreply.github.com>
2021-08-06 22:12:36 +0100

    Revert ":banking: New banking not as buggy"
    This reverts commit 1482ad8f3aa542059e5d1ee43df3030835004eb6.

--- server-data/resources/[PRP]/mrp-banking-ui/fxmanifest.lua
+++ server-data/resources/[PRP]/mrp-banking-ui/fxmanifest.lua
@@ -4,12 +4,11 @@ fx_version 'adamant'
 game 'gta5'
 
 ui_page 'html/ui.html'
+
 files {
 	'html/ui.html',
 	'html/pricedown.ttf',
 	'html/bank-icon.png',
-	'html/logo.png',
-	'html/cursor.png',
 	'html/styles.css',
 	'html/scripts.js',
 	'html/debounce.min.js'
@@ -20,6 +19,3 @@ client_scripts{
     'client/*.lua',
 }
 
-server_scripts{
-    'server/*.lua',
-}
