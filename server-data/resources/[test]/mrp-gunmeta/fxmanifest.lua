11c44f0abf149cbda7fe7b99c8fa7be180890d8a
Malibu-RP <87510116+Malibu-RP@users.noreply.github.com>
2021-08-06 03:20:42 -0500

    :mrp-gunmeta: gun metas

---
+++ server-data/resources/[test]/mrp-gunmeta/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+description 'Weapon Meta Files'
+
+files {
+	'weapons_custom.meta',
+	'weapons_vanilla.meta',
+	'weapons_pistol_mk2.meta',
+	'weaponheavypistol.meta',
+	'weapons_snspistol_mk2.meta',
+	'weapondbshotgun.meta',
+	'weapons_pumpshotgun_mk2.meta',
+	'weaponcomponents.meta'
+}
+
+	data_file 'WEAPONINFO_FILE' 'weapons_custom.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapons_custom.meta'
+	data_file 'WEAPONINFO_FILE' 'weapons_vanilla.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapons_vanilla.meta'
+	data_file 'WEAPONINFO_FILE' 'weapons_pistol_mk2.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapons_pistol_mk2.meta'
+	data_file 'WEAPONINFO_FILE' 'weaponheavypistol.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weaponheavypistol.meta'
+	data_file 'WEAPONINFO_FILE' 'weapons_snspistol_mk2.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapons_snspistol_mk2.meta'
+	data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
+	data_file 'WEAPONINFO_FILE' 'weapons_pumpshotgun_mk2.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapons_pumpshotgun_mk2.meta'
+	data_file 'WEAPONINFO_FILE' 'weapondbshotgun.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapondbshotgun.meta'
