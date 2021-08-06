1482ad8f3aa542059e5d1ee43df3030835004eb6
Evan-1889 <78846695+Evan-1889@users.noreply.github.com>
2021-08-06 20:34:16 +0100

    :banking: New banking not as buggy

--- server-data/resources/[Assets]/banking/fxmanifest.lua
+++ server-data/resources/[Assets]/banking/fxmanifest.lua
@@ -4,11 +4,12 @@ fx_version 'adamant'
 game 'gta5'
 
 ui_page 'html/ui.html'
-
 files {
 	'html/ui.html',
 	'html/pricedown.ttf',
 	'html/bank-icon.png',
+	'html/logo.png',
+	'html/cursor.png',
 	'html/styles.css',
 	'html/scripts.js',
 	'html/debounce.min.js'
@@ -19,3 +20,6 @@ client_scripts{
     'client/*.lua',
 }
 
+server_scripts{
+    'server/*.lua',
+}
