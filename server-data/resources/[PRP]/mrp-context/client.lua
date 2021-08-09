f4d7742feac2db273a55b6641464d6ecb2eca34f
Evan-1889 <78846695+Evan-1889@users.noreply.github.com>
2021-08-10 00:17:13 +0100

    Update client.lua

--- server-data/resources/[PRP]/mrp-context/client.lua
+++ server-data/resources/[PRP]/mrp-context/client.lua
@@ -2,7 +2,7 @@ RegisterNUICallback("dataPost", function(data, cb)
     SetNuiFocus(false)
     TriggerEvent(data.event, data.args)
     cb('ok')
-    PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
+    -- PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
 end)
 
 RegisterNUICallback("cancel", function()
@@ -12,14 +12,14 @@ RegisterNUICallback("cancel", function()
     TriggerEvent('garges:force:clear')
 
 
-    PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
+    -- PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
 end)
 
 
 RegisterNetEvent('mrp-context:sendMenu', function(data)
     if not data then return end
     SetNuiFocus(true, true)
-    PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
+    -- PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
     SendNUIMessage({
         action = "OPEN_MENU",
         data = data
